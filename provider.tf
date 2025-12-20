5468799a5fb8f5f912d4943423ca39a5be0c61cb
RAHUL-ANIGOKAR <rahul.anigolkar1@gmail.com>
2025-12-20 15:35:28 +0530

    DAY-02

--- provider.tf
+++ provider.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# provider "azurerm" {
-#   subscription_id = "your-azure-subscription-id"
-#   client_id = "your-azure-client-id"
-#   client_secret = "your-azure-client-secret"
-#   tenant_id = "your-azure-tenant-id"
-# }
+ provider "azurerm" {
+   subscription_id = "your-azure-subscription-id"
+   client_id = "your-azure-client-id"
+   client_secret = "your-azure-client-secret"
+   tenant_id = "your-azure-tenant-id"
+ }
