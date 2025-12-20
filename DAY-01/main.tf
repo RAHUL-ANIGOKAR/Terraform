3d55b9ed0b54c29ee14f9dde627330c2ed3f4630
RAHUL-ANIGOKAR <rahul.anigolkar1@gmail.com>
2025-12-20 13:05:46 +0530

    DAY-01

--- DAY-01/main.tf
+++ DAY-01/main.tf
@@ -7,4 +7,8 @@ resource "aws_instance" "demo_ec2" {
   ami                     = "ami-02b8269d5e85954ef"
   instance_type           = "t3.micro"
   key_name = "instance-key"
+
+  tags = {
+    Name = "Ec2_at_Terraform"
+  }
 }
