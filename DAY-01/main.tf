26c50c31453345c3eb445c6e035b837e87e44bbe
RAHUL-ANIGOKAR <rahul.anigolkar1@gmail.com>
2025-12-20 13:02:07 +0530

    DAY-01

---
+++ DAY-01/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+resource "aws_instance" "demo_ec2" {
+  ami                     = "ami-02b8269d5e85954ef"
+  instance_type           = "t3.micro"
+  key_name = "instance-key"
+}
