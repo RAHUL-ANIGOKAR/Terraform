dff10e44e14ca023f10ad936447a042855756449
RAHUL-ANIGOKAR <rahul.anigolkar1@gmail.com>
2026-01-01 11:52:41 +0530

    Iac using Modules

---
+++ DAY-05/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "Ec2" {
+  source = "./module/ec2"
+  ami_id = "ami-02b8269d5e85954ef"
+  instance_type = "t3.micro"
+  Key_value = "instance-key"
+  security_group = ["sg-0e4c939636bd0b74c"]
+  aws_region = "ap-south-1"
+  SubNet_Id = "subnet-06647c24e91c50f3b"
+}
